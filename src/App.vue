0804fb52bc56139bff56d98caf2a39764c92176d
ch3ng <ch3ng57@gmail.com>
2020-12-10 21:56:53 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu'
+
+export default {
+  name: 'App',
+  components: {
+    Menu,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
